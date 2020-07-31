9a8689d171442afccc2efda821d45707ff451066
Kik Engineering <engineering@kik.com>
2020-07-31 00:48:05 -0400

    setup: fix url
    GitOrigin-RevId: cb2117edded3840bc476ecb79ed00626080d661f

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description='Agora SDK for Python',
     author='Kik Engineering',
     author_email='engineering@kik.com',
-    url='https://github.com/kincsostyem/kin-python-internal',
+    url='https://github.com/kinecosystem/kin-python',
     license='MIT',
     packages=find_packages(include=["agora", "agora.*"]),
     long_description=open("README.md").read(),
