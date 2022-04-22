f8d605660749d1d6e255a63a48d7aaa4fb4ae4f3
Richard Mands <richard@devwell.org>
2022-04-22 14:05:27 +0100

    0.7.1

--- setup.py
+++ setup.py
@@ -5,10 +5,30 @@ from setuptools import find_packages
 # load VERSION from file
 exec(open('agora/version.py').read())
 
-with open('requirements.txt') as f:
-    requires = f.readlines()
-with open('requirements.dev.txt') as f:
-    tests_requires = f.readlines()
+# Keep this aligned with requires.txt
+requires = [
+    'grpcio==1.34.1',
+    'agora-api==0.26.0',
+    'kin-base==1.4.1',
+    'ed25519==1.4; sys_platform != "win32" and sys_platform != "cygwin"',
+    'pure25519==0.0.1',
+    'protobuf==3.12.2',
+    'pynacl==1.4.0',
+    'base58==2.0.1'
+]
+
+# Keep this aligned with requires.dev.txt
+tests_requires = {
+    'Flask==1.1.2',
+    'grpcio-testing==1.30.0',
+    'pytest==5.4.3',
+    'pytest-cov==2.10.0',
+    'pytest-freezegun==0.4.2',
+    'pytest-mock==3.2.0',
+    'pytest-timeout==1.4.1',
+    'sphinx==3.1.2',
+    'sphinx-autodoc-typehints==1.11.0'
+}
 
 setup(
     name='kin-sdk-v2',
